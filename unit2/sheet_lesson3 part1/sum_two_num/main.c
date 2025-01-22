7d9a8b67aeeb76bae7fac7f9d71456f5aa0f5a31
Radwa Mohamed <123908341+radwaa6@users.noreply.github.com>
2025-01-22 03:27:44 +0200

    Delete unit2 directory

--- unit2/sheet_lesson3 part1/sum_two_num/main.c
+++
@@ -1,9 +0,0 @@
-#include"stdio.h"
-void main(){
-	int num1, num2;
-	printf("Enter two integers : ");
-	fflush(stdin);
-	fflush(stdout);
-	scanf("%d %d",&num1,&num2);
-	printf("sum :%d ",num1+num2);
-}
