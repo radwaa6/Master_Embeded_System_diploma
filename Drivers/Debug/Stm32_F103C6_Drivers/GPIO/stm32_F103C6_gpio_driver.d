33d245e00fc6a3c80e19e072b61a3f5e72fc5b72
Radwa Mohamed <123908341+radwaa6@users.noreply.github.com>
2025-02-10 18:33:13 +0200

    Delete Drivers directory

--- Drivers/Debug/Stm32_F103C6_Drivers/GPIO/stm32_F103C6_gpio_driver.d
+++
@@ -1,8 +0,0 @@
-Stm32_F103C6_Drivers/GPIO/stm32_F103C6_gpio_driver.o: \
- ../Stm32_F103C6_Drivers/GPIO/stm32_F103C6_gpio_driver.c \
- C:/Users/Compu\ Market/STM32CubeIDE/workspace_1.4.0/Drivers/Stm32_F103C6_Drivers/Inc/stm32_F103C6_gpio_driver.h \
- C:/Users/Compu\ Market/STM32CubeIDE/workspace_1.4.0/Drivers/Stm32_F103C6_Drivers/Inc/stm32f103x6.h
-
-C:/Users/Compu\ Market/STM32CubeIDE/workspace_1.4.0/Drivers/Stm32_F103C6_Drivers/Inc/stm32_F103C6_gpio_driver.h:
-
-C:/Users/Compu\ Market/STM32CubeIDE/workspace_1.4.0/Drivers/Stm32_F103C6_Drivers/Inc/stm32f103x6.h:
